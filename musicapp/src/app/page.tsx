a20e70c9d80f13b192f606ee02806250b24dc5ef
HarshNegi0927 <harshnegi0927@gmail.com>
2024-12-22 10:30:59 +0000

    main comiited

--- musicapp/src/app/page.tsx
+++ musicapp/src/app/page.tsx
@@ -1,7 +1,11 @@
 import WhyChooseUs from "@/components/ WhyChooseUs";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import FeaturedCourses from "@/components/ui/FeaturedCourses";
 import HeroSection from "@/components/ui/HeroSection";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import Image from "next/image";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -9,6 +13,10 @@ export default function Home() {
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
    </main>
   );
 }
